11796c274385a36fc8f9529731960ef8bc56bf09
Evan <evanpurcer@gmail.com>
2012-02-16 12:31:55 -0800

    Add require.js and loaders for Backbone and related libraries

--- js/boostrap.js
+++ js/boostrap.js
@@ -1,28 +1,18 @@
 // Filename: bootstrap.js
 
 // Require.js allows us to configure shortcut alias
-// There usage will become more apparent futher along in the tutorial.
 require.config({
   paths: {
+
+    // Modular Backbon loader - loads all Backbone dependencies
+    loader: 'libs/backbone/loader',
+
     jQuery: 'libs/jquery/jquery',
     Underscore: 'libs/underscore/underscore',
     Backbone: 'libs/backbone/backbone'
+
+    templates: '../templates'
   }
-
 });
 
-require([
-
-  // Load our app module and pass it to our definition function
-  'app',
-
-  // Some plugins have to be loaded in order due to there non AMD compliance
-  // Because these scripts are not "modules" they do not pass any values to the definition function below
-  'order!libs/jquery/jquery-min',
-  'order!libs/underscore/underscore-min',
-  'order!libs/backbone/backbone-min'
-], function(App){
-  // The "app" dependency is passed in as "App"
-  // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
-  App.initialize();
-});
+require([ 'app' ], function(App){ App.initialize(); });
