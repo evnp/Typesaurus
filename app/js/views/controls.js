a60a3cf022ed518e21f686dd19cb0246ec067254
Evan Purcer <evanpurcer@gmail.com>
2012-04-19 18:59:18 -0700

    Add auto/hotkey controls and toggle between them. Not yet functional.

--- app/js/views/controls.js
+++ app/js/views/controls.js
@@ -19,8 +19,24 @@ define([
                 _: _
             };
 
-            var compiledTemplate = _.template( controlsTemplate, data );
-            $('#controls').html( compiledTemplate );
+            var compiledTemplate = _.template( controlsTemplate, data ),
+                el = $('#controls');
+
+            el.html( compiledTemplate );
+
+            $('#hotkey-auto-switch input', el).change(function (e) {
+                var id = e.target.id,
+                    hotkey = $('#hotkey-controls', el),
+                    auto = $('#auto-controls', el);
+
+                if (id === 'auto') {
+                    hotkey.fadeOut();
+                    auto.fadeIn();
+                } else if (id === 'hotkey') {
+                    auto.fadeOut();
+                    hotkey.fadeIn();
+                }
+            });
         }
     });
 
