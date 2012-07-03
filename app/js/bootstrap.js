4d24449114eeb8ad705ce8e91ddfc8ebb86927ab
Evan Purcer <evanpurcer@gmail.com>
2012-07-03 11:28:04 -0700

    Add minified versions of all plugins
    Also modify bootstrap.js to reference them correctly

--- app/js/bootstrap.js
+++ app/js/bootstrap.js
@@ -1,11 +1,11 @@
 require.config({
     paths: {
-        jquery: 'libs/jquery/jquery-min',
-        jqhotkeys: 'libs/jquery/plugins/jquery.hotkeys',
-        jqlongkeys: 'libs/jquery/plugins/jquery.longkeys',
-        jqcolor: 'libs/jquery/plugins/jquery.color.min',
+        jquery:     'libs/jquery/jquery-min',
+        jqhotkeys:  'libs/jquery/plugins/jquery.hotkeys.min',
+        jqlongkeys: 'libs/jquery/plugins/jquery.longkeys.min',
+        jqcolor:    'libs/jquery/plugins/jquery.color.min',
         underscore: 'libs/underscore/underscore-1.3.2-amd-min',
-        backbone: 'libs/backbone/backbone-0.9.2-amd-min',
+        backbone:   'libs/backbone/backbone-0.9.2-amd-min',
 
         text: 'libs/require/text',
         templates: '../templates'
