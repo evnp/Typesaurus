1b066116a2654ec0ab194dff4a7ccf2a84835f95
evan <evanpurcer@gmail.com>
2012-09-17 12:18:01 -0700

    Add area.gami library to app

--- app/js/bootstrap.js
+++ app/js/bootstrap.js
@@ -6,6 +6,7 @@ require.config({
         jqcolor:    'libs/jquery/plugins/jquery.color.min',
         underscore: 'libs/underscore/underscore-1.3.2-amd-min',
         backbone:   'libs/backbone/backbone-0.9.2-amd-min',
+        areagami:   'libs/area.gami/area.gami.min',
 
         text: 'libs/require/text',
         templates: '../templates'
